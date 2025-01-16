49dfea0ecde3fe692266c949f992f574f98cb60a
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2025-01-16 14:59:17 +0000

    created new react project

---
+++ first-react/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const productsData = [
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+  ];
+
+  const listItems = productsData.map(product =>
+    <li key={product.name}>
+      {product.name}
+    </li>
+  );
+  
+  return (
+    <>
+      <ul>
+        {listItems}
+      </ul>
+    </>
+  )
+}
+
+export default App
